2dc40de0a587f84ac3a373569a22999a1fbfa462
BalsaBozovicCikom <balsa.bozovic@cikom.com>
2025-05-06 00:30:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -110,4 +110,4 @@ def download():
     return send_file(os.path.join(UPLOAD_FOLDER, 'processed.mp4'), as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
